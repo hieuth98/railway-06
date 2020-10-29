75ed5cdd8ac3c99586b848e28a7c19d61fa183fd
hieuth98 <hieu24129998@gmail.com>
2020-10-29 13:32:46 +0700

    submt assigment 1

---
+++ SQL/Assigment 1/Testing_System_Assignment_1.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE Testing_System;
+USE Testing_System;
+CREATE TABLE Department  (
+	DepartmentID			INT,
+	DepartmentName			VARCHAR(50)
+);
+CREATE TABLE Position (
+	PositionID				INT,
+	PositionName			VARCHAR(50)
+);
+CREATE TABLE  `account`(
+	AccountID				INT,
+    Email					VARCHAR(50),
+    Username				VARCHAR(50),
+    DepartmentID			INT,
+    PositionID				INT,
+    CreateDate				DATE
+    );
+CREATE TABLE `Group` (
+	GroupID					INT,
+    GroupName				VARCHAR(50),
+    CreatorID				INT,
+    CreateDate				DATE
+    );
+CREATE TABLE `GroupAccount` (
+	GroupID					INT,
+    AccountID				INT,
+    JoinDate 				DATE
+    );
+CREATE TABLE TypeQuestion (
+	TypeID					INT
+    );
+    
+
+
