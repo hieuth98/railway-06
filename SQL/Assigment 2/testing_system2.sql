63882942433f40222519c76047352120439b18e7
hieuth98 <hieu24129998@gmail.com>
2020-11-01 10:36:24 +0700

    testing_system2

--- SQL/Assigment 2/testing_system2.sql
+++ SQL/Assigment 2/testing_system2.sql
@@ -3,44 +3,44 @@ CREATE DATABASE testing_system;
 USE testing_system;
 DROP TABLE IF EXISTS department;
 CREATE TABLE department(
-department_id		TINYINT UNSIGNED PRIMARY KEY,
-department_name		VARCHAR(20)
+	department_id		TINYINT UNSIGNED PRIMARY KEY,
+	department_name		VARCHAR(20)
 );
 DROP TABLE IF EXISTS positon;
 CREATE TABLE positon(
-position_id			TINYINT UNSIGNED PRIMARY KEY,
-position_name		ENUM('Dev','Test','Scrum Master','PM')
+	position_id			TINYINT UNSIGNED PRIMARY KEY,
+	position_name		ENUM('Dev','Test','Scrum Master','PM')
 );
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account`(
-account_id			TINYINT UNSIGNED PRIMARY KEY,
-email				VARCHAR(50),
-user_name			VARCHAR(50),
-full_name			VARCHAR(50),
-department_id		TINYINT UNSIGNED,
-position_id			VARCHAR(50),
-create_date			DATE
+	account_id			TINYINT UNSIGNED PRIMARY KEY,
+	email				VARCHAR(50),
+	user_name			VARCHAR(50),
+	full_name			VARCHAR(50),
+	department_id		TINYINT UNSIGNED,
+	position_id			VARCHAR(50),
+	create_date			DATE
 );
 DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group`(
-group_id			TINYINT UNSIGNED,
-group_name			VARCHAR(50),
-creator_id			TINYINT UNSIGNED,
-create_date			DATE
+	group_id			TINYINT UNSIGNED,
+	group_name			VARCHAR(50),
+	creator_id			TINYINT UNSIGNED,
+	create_date			DATE
 );
 DROP TABLE IF EXISTS group_account;
 CREATE TABLE group_account(
-group_id			TINYINT UNSIGNED,
-account_id			TINYINT UNSIGNED,
-join_date			DATE
+	group_id			TINYINT UNSIGNED,
+	account_id			TINYINT UNSIGNED,
+	join_date			DATE
 );
 DROP TABLE IF EXISTS type_question;
 CREATE TABLE type_question(
-type_id				TINYINT UNSIGNED
+	type_id				TINYINT UNSIGNED
 );
 CREATE TABLE category_question(
-    category_id				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    category_name			NVARCHAR(50) NOT NULL UNIQUE KEY
+	category_id				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	category_name			NVARCHAR(50) NOT NULL UNIQUE KEY
 );
 
 -- create table 8: question
@@ -63,7 +63,7 @@ CREATE TABLE answer(
     answers					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     content					NVARCHAR(100) NOT NULL,
     questionID				TINYINT UNSIGNED NOT NULL,
-    is_correct				ENUM('True','False'),
+    is_correct				 ENUM ('True','False'),    
     FOREIGN KEY(question_id) REFERENCES question(question_id)
 );
 
@@ -78,7 +78,7 @@ CREATE TABLE exam(
     creator_id				TINYINT UNSIGNED NOT NULL,
     createdate				DATETIME DEFAULT NOW(),
     FOREIGN KEY(category_id) REFERENCES category_question(category_id),
-    FOREIGN KEY(creator_id) 	REFERENCES `account`(account_id)
+    FOREIGN KEY(creator_id) REFERENCES `account`(account_id)
 );
 
 -- create table 11: exam_question
